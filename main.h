9472be3890629160c5495d7ce992906829a278bd
Unimke1990 <agimunimke1990@gmail.com>
2023-10-13 06:24:42 -0700

    or main. h file

---
+++ main.h
@@ -0,0 +1,11 @@
+#ifndef _MAIN_H
+#define _MAIN_H
+#include <stdarg.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+int _printf(const char *format, ...);
+int m_putchar(char c);
+int my_puts(char *c);
+
+#endif
