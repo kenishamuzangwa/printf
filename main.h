cc394980eb99dc8bef0d69d4aaa9f5f6792f4e66
Unimke1990 <agimunimke1990@gmail.com>
2023-10-13 08:22:22 -0700

    print_c

--- main.h
+++ main.h
@@ -1,6 +1,3 @@
-<<<<<<< HEAD
-
-=======
 #ifndef _MAIN_H
 #define _MAIN_H
 #include <stdarg.h>
@@ -12,4 +9,3 @@ int m_putchar(char c);
 int my_puts(char *c);
 
 #endif
->>>>>>> cc4a394912f3d2a9ac9f18bf97c4cabdbae8ee8b
