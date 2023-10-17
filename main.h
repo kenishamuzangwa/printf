59e816557a31ebe1394e627a1284056e62f255d7
Unimke1990 <agimunimke1990@gmail.com>
2023-10-17 13:03:07 -0700

    update

--- main.h
+++ main.h
@@ -1,17 +1,13 @@
 #ifndef _MAIN_H
 #define _MAIN_H
-
-
 #include <stdarg.h>
 #include <unistd.h>
+#include <stdlib.h>
 
-
-int p_putchar.c(char z);
+int m_putchar(char c);
 int _printf(const char *format, ...);
-int my_puts.c(char *string);
-int put_int(int interger);
-int my_puts.c(char *string);
-int m_putchar.c(char c);
+int my_puts(char *c);
 void get_int(int num);
+
 
 #endif
