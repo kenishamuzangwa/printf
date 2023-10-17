5467b9c3de361e2b90c3e130831d6e458261adb4
kenisha <kenishamuzangwa@gmail.com>
2023-10-17 11:54:14 -0700

    Updated

--- main.h
+++ main.h
@@ -1,11 +1,17 @@
 #ifndef _MAIN_H
 #define _MAIN_H
+
+
 #include <stdarg.h>
 #include <unistd.h>
-#include <stdlib.h>
 
+
+int p_putchar.c(char z);
 int _printf(const char *format, ...);
-int m_putchar(char c);
-int my_puts(char *c);
+int my_puts.c(char *string);
+int put_int(int interger);
+int my_puts.c(char *string);
+int m_putchar.c(char c);
+void get_int(int num);
 
 #endif
