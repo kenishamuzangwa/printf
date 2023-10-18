1e2a9d6a184fede66cc0fe8b5034f1f9015f6712
Unimke1990 <agimunimke1990@gmail.com>
2023-10-17 20:17:08 -0700

    update

--- int_get.c
+++ int_get.c
@@ -1,21 +1,32 @@
 #include "main.h"
-
 /**
- * int_get - print intergers
- *
- * @num: int
- * Return: number of bytes
- */
+* int_get - print integers and return the number of bytes printed
+*
+* @num: int
+* Return: number of bytes
+*/
 
-void int_get(int num)
+int int_get(int num)
 {
 int j;
+int value = 0;
+if (num < 0)
+{
+m_putchar('-');
+value++;
+num = -num;
+}
 
-	if (num == 0)
-	return;
+if (num == 0)
+{
+m_putchar('0');
+return (1);
+}
 
-	j = num / 10;
+j = num / 10;
 
-	int_get(j);
-	m_putchar(num % 10 + '0');
+value += int_get(j);
+m_putchar(num % 10 + '0');
+value++;
+return (value);
 }
